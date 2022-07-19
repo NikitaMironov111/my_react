1a1489018e0506ff508f078f7682c59288694e34
John Doe <111nikail111@gmail.com>
2022-07-19 23:37:47 +0300

    Added ccs rules for ButtonProps

--- src/components/ButtonProps/ButtonProps.tsx
+++ src/components/ButtonProps/ButtonProps.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
-import './ButtonProps.css';
+import React from 'react'
+import './ButtonProps.css'
+const ButtonProps = () => {
+  return (
+    <button className='my-button-props'>Click me please!</button>
+  )
+}
 
-export default ButtonProps;
+export default ButtonProps
