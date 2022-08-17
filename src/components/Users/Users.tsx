69dbb91b3061347ede0e422a404e3cc037f2fb72
John DoeNikita <111nikail111@gmail.com>
2022-08-17 21:47:34 +0300

    realise add user function

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,18 +1,41 @@
-import React, {ChangeEvent, FC, useState} from "react";
+import React, {ChangeEvent, FC, useState, useMemo, FormEvent} from "react";
 import {USERS} from "./usersData";
 import {IUser} from './IUser';
+import { initialUser } from "./initialUser";
 
 const Users:FC = () => {
 	const [users, setUsers] = useState<IUser[]>(USERS);
+	const [search, setSearch] = useState('');
+	const [showUserForm, setShowUserForm] = useState(false);
+	const [user, setUser] = useState(initialUser);
+	const [btnName, setBtnName] = useState('Add new User');
+
 	const deleteUser = (id:number) => {
 		const isDelete = window.confirm('Do you want realy delete?');
 		if(isDelete){
 			setUsers(users.filter(user => user.id !== id));
 		}
 	};
-	const searchUser = (userName:string) => {
-		setUsers(USERS.filter(user => user.name.toLowerCase().includes(userName.toLowerCase())));
+
+	const searchedUsers = useMemo(() => {
+		console.log(users)
+		if(search) {
+			return users.filter(user => user.name.toLowerCase().includes(search.toLowerCase()));
+		}
+		return users;
+	}, [search, users]);
+
+	const onChangeUserData = (event: ChangeEvent<HTMLInputElement>) => {
+		const field = event.target.id;
+		setUser({...user, [field]: event.target.value});
 	};
+
+	const addUser = (event: FormEvent) => {
+		event.preventDefault();
+		setUsers([...users, user]);
+		setUser(initialUser);
+
+	}
 
 	return (
 		<>
@@ -23,20 +46,49 @@ const Users:FC = () => {
 					placeholder="Username" 
 					aria-label="Username"
 					aria-describedby="basic-addon1"
-					onChange={(event) => searchUser(event.target.value)}
+					onChange={(event) => setSearch(event.target.value)}
 				/>
 			</div>
+			<button className="btn btn-success mt-3 mb-3"
+				onClick={() => {
+					setShowUserForm(!showUserForm);
+					}
+				}
+			>
+				{btnName}
+			</button>
+			{showUserForm 
+				&& 
+				<form onSubmit={(event) => addUser(event)}>
+					{Object.keys(user).map(field =>{
+							if(field === 'id' || field === 'address' || field === 'company' || field === 'username') return;
+							return <div className="mb-3" key={field}>
+										<label htmlFor={field} className="form-label">{field}</label>
+										<input type="text" 
+												className="form-control" 
+												id={field}
+												required
+												value={user[field as keyof Omit<IUser, 'id' | 'address' | 'company' | 'username'>]}
+												onChange={(event) => onChangeUserData(event)}
+										/>
+									 </div>
+						}
+					)}
+
+					<button type="submit" className="btn btn-primary mb-2">Add</button>
+				</form>
+			}
 			<div className="row row-cols-1 row-cols-md-3 g-4">
-				{users.length 
+				{searchedUsers.length 
 					? 
-					users.map(user =>
+					searchedUsers.map(user =>
 						<div className="col" key={user.id}> 
 							<div className="card h-100">
 								<div className="card-body">
 									<h5 className="card-title">{`#${user.id} - ${user.name}`}</h5>
 									<p className="card-text">E-mail: {user.email}</p>
-									<p className="card-text">City: {user.address.city}</p>
-									<p className="card-text">Company: {user.company.name}</p>
+									<p className="card-text">Phone: {user.phone}</p>
+									<p className="card-text">WebSite: {user.website}</p>
 								</div>
 								<div className="card-footer">
 									<button type="button" className="btn btn-danger" 
